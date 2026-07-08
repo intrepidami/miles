c1627d2c3ddfb42696d8441754f8d5e1edb1cf8a
chenyx <yuxinchen@mail.sdu.edu.cn>
2026-07-08 10:59:00 +0800

    Disable rollout-shuffle for match mode

--- scripts/run_qwen3_4b.py
+++ scripts/run_qwen3_4b.py
@@ -123,7 +123,7 @@ def execute(args: ScriptArgs):
         "--apply-chat-template "
         # By default it is thinking mode
         # """--apply-chat-template-kwargs '{"enable_thinking":false}' """
-        "--rollout-shuffle "
+        f"{'--rollout-shuffle ' if not is_match else ''}"
         "--rm-type math "
         f"--num-rollout {2 if is_match else (debug_num_rollout if is_debug_one_sample else 3000)} "
         f"--rollout-batch-size {128 if is_match else (1 if is_debug_one_sample else 32)} "
