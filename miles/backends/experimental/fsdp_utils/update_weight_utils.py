db60546930e62a72b109eed14708fe25a0ac9980
chenyx <yuxinchen@mail.sdu.edu.cn>
2026-07-09 15:36:54 +0800

    fsdp update_weights: open/close SGLang weight-update session around tensor sync

--- miles/backends/experimental/fsdp_utils/update_weight_utils.py
+++ miles/backends/experimental/fsdp_utils/update_weight_utils.py
@@ -52,6 +52,9 @@ class UpdateWeight(abc.ABC):
             futures = [engine.pause_generation.remote() for engine in self.rollout_engines]
             futures.extend([engine.flush_cache.remote() for engine in self.rollout_engines])
             ray.get(futures)
+            # SGLang requires an open weight-update session before
+            # update_weights_from_tensor (same protocol as the Megatron path).
+            ray.get([engine.begin_weight_update.remote() for engine in self.rollout_engines])
         dist.barrier(group=get_gloo_group())
 
         bucket = []
@@ -82,6 +85,7 @@ class UpdateWeight(abc.ABC):
 
         dist.barrier(group=get_gloo_group())
         if dist.get_rank() == 0:
+            ray.get([engine.end_weight_update.remote() for engine in self.rollout_engines])
             ray.get([engine.continue_generation.remote() for engine in self.rollout_engines])
         dist.barrier(group=get_gloo_group())
 
